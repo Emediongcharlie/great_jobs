6c4767fece7a7fada30437bbac5c68d3cbc53fbb
Emediongcharlie <159178054+Emediongcharlie@users.noreply.github.com>
2024-04-03 13:43:56 +0100

    Update table8.py

--- Python Exercises/Exercises 2/table8.py
+++ Python Exercises/Exercises 2/table8.py
@@ -2,7 +2,7 @@
 print(number\t\t square\t\t cube\t\t)
 
 for number in range(0,6)
-square = number ** 2
-cube = number ** 3
+    square = number ** 2
+    cube = number ** 3
 
-print(f"{number} {square} {cube}")
+    print(f"{number} {square} {cube}")
