d4e85b488fce5c0de63dc2cef4b122807a13280e
Emediong Charlie <emediongcharliegmail.com>
2024-04-06 00:16:57 +0100

    Minimum and Maximum calculations

---
+++ Python Exercises/minimum_maximum.py
@@ -0,0 +1,12 @@
+number1 = int(input("Enter number:"))
+number2 = int(input("Enter number2:"))
+number3 = int(input("Enter number3:"))
+
+minimum = number1
+if	(number2 < minimun):
+	minimum = number2
+
+if	(number3 < minimum):
+	minimum = number3
+
+print('Minimum value is', minimum)
