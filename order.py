e90d8e4dcf7afaad2fcdd3c5824ca8f52f2263d1
Emediong Charlie <emediongcharliegmail.com>
2024-04-05 03:22:56 +0100

    python file

---
+++ order.py
@@ -0,0 +1,35 @@
+order = ord('A')
+print(order)
+
+order = ord('B')
+print(order)
+
+order = ord('C')
+print(order)
+
+order = ord('D')
+print(order)
+
+order = ord('c')
+print(order)
+
+order = ord('d')
+print(order)
+
+order = ord('0')
+print(order)
+
+order = ord('1')
+print(order)
+
+order = ord('2')
+print(order)
+
+order = ord('$')
+print(order)
+
+order = ord('*')
+print(order)
+
+order = ord('+')
+print(order)
