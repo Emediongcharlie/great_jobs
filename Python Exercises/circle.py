0b53738be428490c6ff5e990fef5bd8ea016f8e4
Emediong Charlie <emediongcharliegmail.com>
2024-04-06 00:16:07 +0100

    calculations on circle

---
+++ Python Exercises/circle.py
@@ -0,0 +1,10 @@
+radius = int(input("Enter the radius of the number"))
+pie = 3.14159
+
+diameter = 2*radius
+circumference = 2*pie*radius
+area = pie* (radius*radius)
+
+print(diameter)
+print(circumference)
+print(area)
