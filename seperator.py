7c1d98aab14be7cc56676060f11b36ba1ab5bec6
Emediong Charlie <emediongcharliegmail.com>
2024-04-05 03:24:44 +0100

    Python file

---
+++ seperator.py
@@ -0,0 +1,9 @@
+#number = 42339
+#string = str(number)
+#list_of_digits = []
+#for i in string:
+#	list_of_digits.append(int(i))
+#	print(list_of_digits)
+
+for digit in '42339':
+	print(digit, end="  ")
