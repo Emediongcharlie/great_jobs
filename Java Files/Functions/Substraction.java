b070520220b3b9cc21ef51bf683ab765ff0b2ec5
Emediong Charlie <emediongcharliegmail.com>
2024-04-08 07:11:48 +0100

    java function

---
+++ Java Files/Functions/Substraction.java
@@ -0,0 +1,9 @@
+public class Substraction{
+	public static int substract(int number, int number2){
+		return Math.abs(number - number2);
+	}
+
+public static void main(String[] args){
+	System.out.println(substract(10,15));
+}
+}
