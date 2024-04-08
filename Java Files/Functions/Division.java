daafac2161690513ecf9a775db253574e25087b0
Emediong Charlie <emediongcharliegmail.com>
2024-04-08 07:07:50 +0100

    java function

---
+++ Java Files/Functions/Division.java
@@ -0,0 +1,13 @@
+public class DivisionOne {
+	static int divide(int number, int number2) {
+		int division = number / number2;
+		return division;
+
+		}
+	
+	public static void main(String[] args) {
+	float result = divide(4,5);
+	System.out.println(result);
+	}
+	}
+	
