93cabee4d5e2624910639875f9b8b5eabff929d4
Emediong Charlie <emediongcharliegmail.com>
2024-04-08 07:09:40 +0100

    java function

---
+++ Java Files/Functions/EvenOrOdd.java
@@ -0,0 +1,17 @@
+public class EvenOrOdd{
+	static void even(int number){
+		if (number % 2 == 0){
+			System.out.print("True");
+		}
+		else {
+			System.out.print("False");
+		}
+	}
+
+public static void main(String[] args){
+	even(43);
+	}
+}
+
+	
+	
---
+++ Java Files/Functions/Factorial.java
@@ -0,0 +1,17 @@
+public class Factorial{
+	static int factorial(int num){
+	
+	if(num == 1){
+	return 1;
+	}
+	else{
+	return num * factorial(num-1);
+	
+	}
+	
+}
+
+public static void main(String[] args){
+	System.out.print(factorial(10));
+}
+}
