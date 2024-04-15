759cb0aaac721a96f340aa9e1cd2dfcbfc459070
Emediong Charlie <emediongcharliegmail.com>
2024-04-15 21:44:09 +0100

    array

---
+++ Java Files/PartOneArray2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+	public class PartOneArray2{
+	public static void main(String[] args){
+
+	Scanner input = new Scanner(System.in);
+	
+	String [] names = new String [5];
+
+	for(int i = 0; i < 5; i++){
+	System.out.print("Enter name:");
+	String name = input.nextLine();
+	names[counter] = name;
+	}
+
+	for(int i = 0; i < 5; i++){
+	System.out.print(names[counter])
+	}
+}
+}
